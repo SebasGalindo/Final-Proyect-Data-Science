bd8d382584798379b7168ec7a0a4864546913473
MiguelMorenoB <167891980+MiguelMorenoB@users.noreply.github.com>
2024-05-13 16:52:04 -0500

    first stable version

---
+++ Vulns_Dashboard/app/config/config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config:
+    CONTAINER_MACHINES = os.getenv('CONTAINER_MACHINES', 'default_url_machines')
+    CONTAINER_SOFTWARES = os.getenv('CONTAINER_SOFTWARES', 'default_url_softwares')
+    CONTAINER_CVES = os.getenv('CONTAINER_CVES', 'default_url_cves')
+    API_KEY_CVEDETAILS = os.getenv('API_KEY_CVEDETAILS', 'default_api_key')
+    API_KEY_NIST = os.getenv('API_KEY_NIST', 'default_api_key')
+    API_URL = os.getenv('API_URL', 'default_api_url')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/defaultdb')
+    MONGO_VULNS_KEY = os.getenv('MONGO_VULNS_KEY', 'default_mongo_key')
+    MONGO_VULNS_URL = os.getenv('MONGO_VULNS_URL', 'default_mongo_url')
