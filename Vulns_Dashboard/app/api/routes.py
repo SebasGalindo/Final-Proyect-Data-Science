c033be54203a5e5c611ccea1ba00ad6fc0125f08
MiguelMorenoB <167891980+MiguelMorenoB@users.noreply.github.com>
2024-05-16 02:18:54 -0500

    ADD GRAPHICS AND INDICATORS

--- Vulns_Dashboard/app/api/routes.py
+++ Vulns_Dashboard/app/api/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, jsonify
-from .utilities import get_vulnerable_softwares 
+from flask import Blueprint, jsonify, request
 from app.extensions import mongo
+from .utilities import *
+from datetime import datetime
 
 api_blueprint = Blueprint('api', __name__)
 
@@ -9,13 +10,21 @@ def get_data():
     data = {"message": "Pong"}
     return jsonify(data), 200
 
-@api_blueprint.route('/get_vuln_softwares', methods=['GET'])
-def get_vuln_softwares():
-    try:
-        data = get_vulnerable_softwares()
-        if not data:  
-            return jsonify({'message': 'No vulnerable software found'}), 404
-        return jsonify(data), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
+@api_blueprint.route('/all_data', methods=['GET'])
+def get_all_data():
+    current_year = request.args.get('year', datetime.now().year)
+    vulnerable_softwares = get_vulnerable_softwares()
+    
+    data = {
+        "top_ten_vulnerable_softwares": chart_top_ten_vulnerable_softwares(),
+        "impact_vulnerabilities": impact_vulnerabilities(vulnerable_softwares),
+        "recent_vulnerabilities_qty": recent_vulnerabilities_qty(str(current_year)),
+        "most_vulnerable_machine": most_vulnerable_machine(),
+        "qty_critical_vulns": qty_critical_vulns(vulnerable_softwares),
+        "top_ten_riskiest_vulnerabilities": chart_top_ten_riskiest_vulnerabilities(),
+        "qty_vulns_severity_across_time": qty_vulns_severity_across_time(vulnerable_softwares),
+        "machines_most_vuln": donut_chart_machines_most_vuln(),
+        "cves_version3": radial_chart_cves_version3()
+    }
+    
+    return jsonify(data)
--- Vulns_Dashboard/app/dashboard/controllers.py
+++ Vulns_Dashboard/app/dashboard/controllers.py
@@ -1,7 +1,32 @@
+import requests
 from flask import Blueprint, render_template
+from datetime import datetime
 
 dashboard_blueprint = Blueprint('dashboard', __name__)
 
+API_BASE_URL = "http://localhost:5000/api"
+
 @dashboard_blueprint.route('/')
 def dashboard_home():
-    return render_template('index.html')
+    current_year = datetime.now().year
+    response = requests.get(f"{API_BASE_URL}/all_data", params={'year': current_year})
+
+    data = response.json()
+
+    top_ten_chart = data.get("top_ten_vulnerable_softwares")
+    impact_chart = data.get("impact_vulnerabilities")
+    riskiest_chart = data.get("top_ten_riskiest_vulnerabilities")
+    severity_across_time_chart = data.get("qty_vulns_severity_across_time")
+    machines_most_vuln_chart = data.get("machines_most_vuln")
+    cves_version3_chart = data.get("cves_version3")
+    
+    recent_vulns_qty = data.get("recent_vulnerabilities_qty", 0)
+    most_vulnerable_machine = data.get("most_vulnerable_machine", {}).get('vuln_softwares_qty', 0)
+    qty_critical_vulns = data.get("qty_critical_vulns", 0)
+
+    chart_data = [
+        top_ten_chart, impact_chart, riskiest_chart, 
+        severity_across_time_chart, cves_version3_chart, machines_most_vuln_chart
+    ]
+
+    return render_template('index.html', chart_data=chart_data, recent_vulns_qty=recent_vulns_qty, current_year=current_year, most_vulnerable_machine=most_vulnerable_machine, qty_critical_vulns=qty_critical_vulns)
